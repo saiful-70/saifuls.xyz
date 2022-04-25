50a2cb4508808db85a204ec034c336db0c0d901f
Saiful Islam <saiful70.me@gmail.com>
2022-04-25 19:20:33 +0600

    link deleted from card and added facebook link in about

--- components/homepage/social-icons.tsx
+++ components/homepage/social-icons.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import { AiOutlineGithub, AiOutlineTwitter, AiFillLinkedin } from 'react-icons/ai';
+import { AiOutlineGithub, AiOutlineTwitter, AiFillLinkedin, AiFillFacebook } from 'react-icons/ai';
 
 const SocialIcons = () => {
   return (
     <Box display="flex" justifyContent="center" alignItems="flex-end">
+      <Box
+        as="a"
+        color="#273c75"
+        fontSize={['1.4rem', '2.4rem', '3rem', '3rem']}
+        mt="2.5rem"
+        mr="20px"
+        href="https://facebook.com/saiful70.me">
+        <AiFillFacebook />
+      </Box>
       <Box
         as="a"
         fontSize={['1.4rem', '2.4rem', '3rem', '3rem']}
@@ -24,6 +33,7 @@ const SocialIcons = () => {
       </Box>
       <Box
         as="a"
+        color="#0c66c2"
         fontSize={['1.4rem', '2.4rem', '3rem', '3rem']}
         mt="2.5rem"
         href="https://www.linkedin.com/in/saiful70/">
--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -6,48 +6,48 @@ import Link from 'next/link';
 
 const ProjectCard = ({ project }) => {
   return (
-    <Link href={project.github}>
-      <Box
-        maxW={'350px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'md'}
-        rounded={'md'}
-        p={6}
-        m="1rem"
-        cursor="pointer"
-        overflow={'hidden'}>
-        <Stack>
-          <Text
-            color="project"
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-            letterSpacing={1.1}>
-            Project
-          </Text>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'2xl'}
-            fontFamily={'body'}>
-            {project.name}
-          </Heading>
-          <Text color={'gray.500'}>{project.description}</Text>
-          <Box display="flex">
-            {project.demo && (
-              <Box as="a" href={`https://${project.demo}`} mr="1rem">
-                Preview
-              </Box>
-            )}
-            {project.github && (
-              <Box as="a" href={project.github} fontSize="1.6rem">
-                <AiFillGithub />
-              </Box>
-            )}
-          </Box>
-        </Stack>
-      </Box>
-    </Link>
+    // <Link href={project.github}>
+    <Box
+      maxW={'350px'}
+      w={'full'}
+      bg={useColorModeValue('white', 'gray.900')}
+      boxShadow={'md'}
+      rounded={'md'}
+      p={6}
+      m="1rem"
+      cursor="pointer"
+      overflow={'hidden'}>
+      <Stack>
+        <Text
+          color="project"
+          textTransform={'uppercase'}
+          fontWeight={800}
+          fontSize={'sm'}
+          letterSpacing={1.1}>
+          Project
+        </Text>
+        <Heading
+          color={useColorModeValue('gray.700', 'white')}
+          fontSize={'2xl'}
+          fontFamily={'body'}>
+          {project.name}
+        </Heading>
+        <Text color={'gray.500'}>{project.description}</Text>
+        <Box display="flex">
+          {project.demo && (
+            <Box as="a" href={`${project.demo}`} target="_blank" mr="1rem" fontWeight="bold">
+              Preview
+            </Box>
+          )}
+          {project.github && (
+            <Box as="a" href={project.github} fontSize="1.6rem">
+              <AiFillGithub />
+            </Box>
+          )}
+        </Box>
+      </Stack>
+    </Box>
+    // </Link>
   );
 };
 
