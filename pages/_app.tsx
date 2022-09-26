e41c61fbcbd6841e881393abc9ce739da8783ef8
Saiful Islam <saiful70.me@gmail.com>
2022-09-27 00:33:40 +0600

    script changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,11 +14,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="afterInteractive" id="analytics">
+      <Script strategy="afterInteractive" id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
+        
           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
