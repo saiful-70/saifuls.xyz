1013633cbda9ed6a93e9508a5b143ee2e4434077
Saiful Islam <saiful70.me@gmail.com>
2022-12-19 23:18:45 +0600

    deleted programming language section

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,14 +4,12 @@ import Header from "../../components/About/Header";
 import Skills from "../../components/About/Skills";
 import Education from "../../components/About/Education";
 import ProblemSolvingSkills from "../../components/About/ProblemSolvingSkills";
-import ProgrammingLanguages from "../../components/About/ProgrammingLanguages";
 
 const Index: NextPage = () => {
   return (
     <React.Fragment>
       <Header />
       <Skills />
-      {/* <ProgrammingLanguages /> */}
       <ProblemSolvingSkills />
       <Education />
     </React.Fragment>
