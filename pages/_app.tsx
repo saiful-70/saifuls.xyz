2ece3b789b40e91e68b6420f8cd08aa02b7bfa83
Saiful Islam <saiful70.me@gmail.com>
2022-12-19 13:05:22 +0600

    comment deleted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,13 +34,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
-// <!-- Google tag (gtag.js) -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=G-2N74T6GHQ4"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-
-//   gtag('config', 'G-2N74T6GHQ4');
-// </script>
